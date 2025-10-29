94570b26bf774e6c1b77f52c60fad9ad626213f9
Saloni9989 <155825936+Saloni9989@users.noreply.github.com>
2025-10-29 20:25:05 +0530

    Add LoginServlet for user authentication

---
+++ Part A: LoginServlet.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        // Hardcoded credentials
+        if ("admin".equals(username) && "1234".equals(password)) {
+            out.println("<h2>Welcome, " + username + "!</h2>");
+        } else {
+            out.println("<h2>Login Failed!</h2>");
+            out.println("<a href='login.html'>Try Again</a>");
+        }
+    }
+}
